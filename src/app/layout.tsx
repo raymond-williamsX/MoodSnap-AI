da6fdd1f44735ad8f41e1e518b1caaf9133ceceb
Raymond Williams <msuraymond@gmail.com>
2025-11-24 00:55:02 +0100

    Updated Favicon and Added Google SEO tag in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+      <meta name="google-site-verification" content="mazxvnU0eHQaitoSxtpQtK2RlxtVHLhjW2mLoCPMaaE" />
       </head>
       <body className="font-body antialiased">
         {children}
